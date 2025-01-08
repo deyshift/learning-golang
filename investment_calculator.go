45d1c98a5644f09288bcce5577f159148757ea53
Deysha Rivera <rivera.dsm@gmail.com>
2025-01-08 15:01:02 -0500

    adding rudimentary investment calculation

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,8 +1,18 @@
 package main // designates this file as the main entry point of the program
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+// imports the fmt package for formatted I/O
+
+// main entry point of the program, execution starts here
 func main() {
-	// main entry point of the program, execution starts here
-	fmt.Print("Hello, World!")
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Printf("The future value of the investment is: %.2f\n", futureValue)
 }
