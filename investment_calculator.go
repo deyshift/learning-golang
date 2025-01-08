6c6e391a84453204e511911bec9f4dbdf8f7fd50
Deysha Rivera <rivera.dsm@gmail.com>
2025-01-08 15:34:07 -0500

    add user inputs for years and expected return rate

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,12 +8,18 @@ import (
 // main entry point of the program, execution starts here
 func main() {
 	const inflationRate = 2.5    // use const keyword to declare a constant
-	var investmentAmount float64 // use var keyword to declare a variable and specify the type
-	years := 10.0                // use assignment operator to infer type and initialize the variable
-	expectedReturnRate := 5.5
+	var investmentAmount float64 // use var keyword to declare a variable and specify the type when not initializing
+	var years float64
+	expectedReturnRate := 5.5 // use assignment operator to infer type and initialize the variable - kept this for example
 
 	fmt.Print("Enter the investment amount: ") // use fmt.Println to print a message to the console
 	fmt.Scan(&investmentAmount)                // use fmt.Scan to read input from the user
+
+	fmt.Print("Enter the number of years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
