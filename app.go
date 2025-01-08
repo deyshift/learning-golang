c4fe7e2adab0346c4b81198ee6d089db78cbc4ce
Deysha Rivera <rivera.dsm@gmail.com>
2025-01-08 14:36:46 -0500

    creating app

---
+++ app.go
@@ -0,0 +1,8 @@
+package main // designates this file as the main entry point of the program
+
+import "fmt"
+
+func main() {
+	// main entry point of the program, execution starts here
+	fmt.Print("Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RivTechProjects/golangprog
+
+go 1.23.4
