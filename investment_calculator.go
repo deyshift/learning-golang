c240206b243361277d5d6078b6a0b48d8b0e51e9
Deysha Rivera <rivera.dsm@gmail.com>
2025-01-08 15:29:06 -0500

    add user input for investmentAmount

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,10 +7,13 @@ import (
 
 // main entry point of the program, execution starts here
 func main() {
-	const inflationRate = 2.5           // use const keyword to declare a constant
-	var investmentAmount float64 = 1000 // use var keyword to declare a variable and specify the type
-	years := 10.0                       // use assignment operator to infer type and initialize the variable
+	const inflationRate = 2.5    // use const keyword to declare a constant
+	var investmentAmount float64 // use var keyword to declare a variable and specify the type
+	years := 10.0                // use assignment operator to infer type and initialize the variable
 	expectedReturnRate := 5.5
+
+	fmt.Print("Enter the investment amount: ") // use fmt.Println to print a message to the console
+	fmt.Scan(&investmentAmount)                // use fmt.Scan to read input from the user
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
