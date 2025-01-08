7d40b334c1bc8869f9ddb633b829b0c2021bb58d
Deysha Rivera <rivera.dsm@gmail.com>
2025-01-08 15:05:02 -0500

    add types to avoid inferred type conversions and make code more readable

--- investment_calculator.go
+++ investment_calculator.go
@@ -9,10 +9,10 @@ import (
 
 // main entry point of the program, execution starts here
 func main() {
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000
 	var expectedReturnRate = 5.5
-	var years = 10
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Printf("The future value of the investment is: %.2f\n", futureValue)
 }
