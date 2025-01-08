0872f426e3b3c512a3c0c1913c8e27285b3e77ac
Deysha Rivera <rivera.dsm@gmail.com>
2025-01-08 15:14:10 -0500

    learn to use assignment operators to infer types and initialize variables, add strict typing to declared variable

--- investment_calculator.go
+++ investment_calculator.go
@@ -9,10 +9,10 @@ import (
 
 // main entry point of the program, execution starts here
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	var investmentAmount float64 = 1000 // use var keyword to declare a variable and specify the type
+	years := 10.0                       // use assignment operator to infer type and initialize the variable
+	expectedReturnRate := 5.5
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Printf("The future value of the investment is: %.2f\n", futureValue)
 }
