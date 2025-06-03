46c2ccbe1ab5d9982c22d854b473dfc128a041c8
RivTech <50204868+RivTechProjects@users.noreply.github.com>
2025-06-02 23:06:47 -0400

    issubsequence

---
+++ DSAs/two_pointer/is_subsequence.go
@@ -0,0 +1,30 @@
+package main
+
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	sIndex := 0
+
+	for _, char := range t {
+		if sIndex < len(s) && rune(s[sIndex]) == char {
+			sIndex++
+			if sIndex == len(s) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+	// Example Usage
+	println(isSubsequence("ace", "abcde"))  // Output: true
+	println(isSubsequence("aec", "abcde"))  // Output: false
+	println(isSubsequence("", "abcde"))     // Output: true
+	println(isSubsequence("ace", ""))       // Output: false
+	println(isSubsequence("abc", "aabbcc")) // Output: true
+	println(isSubsequence("axc", "ahbgdc")) // Output: false
+}
